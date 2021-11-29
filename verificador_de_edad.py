64b9c534efc055809f5073f681913e97410d3632
dayanm20 <dayanalexismanrique@hotmail.com>
2021-11-28 23:33:47 -0500

    aprendi a usar los condicionales en pyton con un ejercicio de edad

---
+++ verificador_de_edad.py
@@ -0,0 +1,17 @@
+# edad = int(input("Escribe tu edad: "))
+
+# if edad >= 18:
+#     print("eres mayor de edad")
+# else:
+#     print("eres mayor de edad")
+
+numero = int(input("Escribe un numero"))
+
+if numero > 5:
+    print("es mayor a 5")
+elif numero  ==5:
+    print("es igual a 5")
+else:
+    print("es menor a 5")
+
+
